c533d9708c68af24b55988aca11c9062f864312c
Oscar Forero <oscar.forero@computer.org>
2014-05-25 22:19:51 +0800

    Exploratory DA - Homework 2 - Plot 4

---
+++ ExploratoryDA/Project2/plot4.R
@@ -0,0 +1,27 @@
+
+if(! exists("pollution")) {
+  pollution <- readRDS("summarySCC_PM25.rds")
+  pollution$fips <- as.factor(pollution$fips)
+  pollution$Pollutant <- as.factor(pollution$Pollutant)
+  pollution$year <- as.factor(pollution$year)
+}
+
+if(! exists("class")) {
+  class <- readRDS("Source_Classification_Code.rds")
+}
+
+coalSCC <- class[grep("*[Cc][Oo][Aa][Ll]*",class$Short.Name), "SCC"] 
+coalRelated <- pollution[pollution$SCC %in% coalSCC,]
+
+png(filename="figure/plot4.png", bg="transparent",
+    width=480, height = 480)
+
+emissionsPerYear <- tapply(coalRelated$Emissions, coalRelated$year, sum)
+
+barplot(emissionsPerYear,
+        main="Coal Related Emissions Per Year", 
+        xlab="Year", ylab="Tons")
+
+dev.off()
+
+
